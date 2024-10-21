f41a7a15ffea1932860231d33466f2d7761d477e
victornonso <victornonso44@gmail.com>
2024-10-21 14:49:11 +0100

    added vercel.json, updated wsgi.py file

--- gis_web_api/settings.py
+++ gis_web_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['victornonso.pythonanywhere.com']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
