9eefcd1dddc662bdcb84bce91983800325bd8223
victornonso <victornonso44@gmail.com>
2024-10-21 13:38:10 +0100

    updated allowed host for production

--- gis_web_api/settings.py
+++ gis_web_api/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'FALSE'
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://victornonso.pythonanywhere.com/']
 
