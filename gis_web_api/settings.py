ebcb9204747cc4dca62b6c5e5f4ecb375938b7a1
victornonso <victornonso44@gmail.com>
2024-10-21 13:50:09 +0100

    updated allowed host for production

--- gis_web_api/settings.py
+++ gis_web_api/settings.py
@@ -134,7 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/gis_app/static/'
+# STATIC_URL = '/gis_app/static/'
+
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'gis_web_api/static')
 
 
