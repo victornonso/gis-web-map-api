fa68a7f1b2be58fd1709a7c3b4172c6a047d1876
victornonso <victornonso44@gmail.com>
2024-10-24 23:10:48 +0100

    rmf

--- gis_web_api/settings.py
+++ gis_web_api/settings.py
@@ -31,12 +31,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-DEBUG = True
 
-# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
